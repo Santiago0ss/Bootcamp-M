ee98c7b0b35e04f6c7316f32c02ad9b93f2e47ac
Santiago Salazar <santiago1234ocampo@gmail.com>
2022-07-27 16:45:23 -0300

    Estados de los componentes

--- parte/courseinfo/src/index.js
+++ parte/courseinfo/src/index.js
@@ -1,49 +1,41 @@
-import React from 'react';
 import ReactDOM from 'react-dom';
+import { useState } from 'react';
 
-const Title = (props) => {
-  return <h1>{props.course}</h1>
-}
-const Part1 = (props) => {
-  return <p>{props.part1}</p>
-}
-const Exercise1 = (props) => {
-  return <p>{props.exercises1}</p>
-}
-const Part2 = (props) => {
-  return <p>{props.part2}</p>
-}
-const Exercise2 = (props) => {
-  return <p>{props.exercises2}</p>
-}
-const Part3 = (props) => {
-  return <p>{props.part3}</p>
-}
-const Exercise3 = (props) => {
-  return <p>{props.exercises3}</p>
-}
+const rootElement = document.getElementById("root");
 
-const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+const App = (props) => {
+  const [contadorValue, updateContador] = useState(0);
+  // const contador = useState(0)
+  // const contadorValue = contador[0]
+  // const updateContador = contador[1]
+  const handleClick = () => {
+    updateContador(prevContador => prevContador + 1)
+  }
+  const createDecrecion = () => {
+    updateContador(prevContador => prevContador - 1)
+  }
 
-  return (
-    <div>
-      <Title course={course} />
-      <Part1 part1={part1} />
-      <Exercise1 exercises1={exercises1} />
-      <Part2 part2={part2} />
-      <Exercise2 exercises2={exercises2} />
-      <Part3 part3={part3} />
-      <Exercise3 exercises3={exercises3} />
-      <p>Number of exercises {exercises1 + exercises2 + exercises3}</p>
-    </div>
-  )
+  const handleClickReset = () => {
+    updateContador(0)
+  }
+  const isEven = contadorValue % 2 === 0;
+  const mensajePar = isEven ? "Es par" : "Es impar";
+
+  return <div>
+    <p>El contador es:</p>
+    <h1>{contadorValue}</h1>
+    <p>{mensajePar}</p>
+    <button onClick={handleClick}>
+      Incrementar
+      </button>
+    <button onClick={createDecrecion}>
+      Decrementar
+    </button>
+    <button onClick={handleClickReset}>
+      Reset
+    </button>
+  </div>
 }
 
-ReactDOM.render(<App />, document.getElementById('root'))
+ReactDOM.render(<App />, rootElement);
+
